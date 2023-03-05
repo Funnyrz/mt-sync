576f8fa11cfde7c9c5c33bdc19a790895a37d2d6
付杨阳 <746108479@qq.com>
2023-03-05 17:09:19 +0800

    frist commit

---
+++ capacitor.config.js
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config = {
+  appId: "mt.sync",
+  appName: "mt-sync",
+  webDir: "dist",
+  bundledWebRuntime: false,
+};
+
+export default config;
